fd9ea1ec74fc3216379480630efab574295b86a5
Marcus <marc303i@stud.kea.dk>
2023-02-21 22:42:57 +0100

    Tester brug af billeder over azure

---
+++ js/operator.js
@@ -0,0 +1,38 @@
+let a = 1
+console.log("a = " + a)
+a++
+console.log("a = " + a)
+++a
+console.log("a = " + a)
+console.log("a = " + a++)
+console.log("a = " + ++a)
+
+let b = 2
+console.log("b^3 + 2 = " + (b**3+2))
+
+// left to right
+console.log(25-10-5)
+
+// right to left, assignment
+let dyt = 1 + 2 + 3
+let x,y
+x = y = 20 - 10 - 5
+console.log(x,y)
+
+let minus = -1
+let plus = 5
+console.log(minus)
+let  asci = +"b"
+console.log(typeof asci)
+console.log(asci)
+console.log(typeof 5 + "5")
+console.log(typeof typeof 5)
+
+if (typeof +"5" == "number") console.log('+"5" er et number')
+console.log("4" + 1 > 4 + 1)
+console.log("4" + 1 > 40)
+
+console.log("4" == 4)
+console.log("4" === 4)
+
+
